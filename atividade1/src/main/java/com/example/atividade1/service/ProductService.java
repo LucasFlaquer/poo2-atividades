225a0d5211203d7f89eb885b905ed6f022e82ff9
lucasflaquer <lucas.flaquer@gmail.com>
2020-03-13 00:01:27 -0300

    #1h1g6w[REVISÃO] ProductService + tips in readme

---
+++ atividade1/src/main/java/com/example/atividade1/service/ProductService.java
@@ -0,0 +1,50 @@
+package com.example.atividade1.service;
+
+import java.util.ArrayList;
+
+import com.example.atividade1.model.Product;
+import com.example.atividade1.repository.ProductRepository;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * ProductService
+ */
+@Service
+public class ProductService {
+
+    private ProductRepository pr;
+    private ArrayList<Product> list = new ArrayList<Product>();
+    public Product getProductById(int id) {
+        Product prod = pr.getProductById(id);
+        return prod;
+    }
+    public ArrayList<Product> getProductsWithStock() {
+        ArrayList<Product> prodList = pr.getProducts();
+        this.list.clear();
+
+        for (Product prod : prodList) {
+            if(prod.getStock() > 0)
+                this.list.add(prod);
+        }
+        return this.list;
+    }
+    public ArrayList<Product> getProductsByValueUp(double value){
+        ArrayList<Product> prodList = pr.getProducts();
+        this.list.clear();        
+        for (Product prod : prodList) {
+            if(prod.getPrice() > value)
+                this.list.add(prod);
+        }
+        return this.list;
+    }
+    public ArrayList<Product> getProductsByValueDown(double value){
+        ArrayList<Product> prodList = pr.getProducts();
+        this.list.clear();        
+        for (Product prod : prodList) {
+            if(prod.getPrice() < value)
+                this.list.add(prod);
+        }
+        return this.list;
+    }
+}
