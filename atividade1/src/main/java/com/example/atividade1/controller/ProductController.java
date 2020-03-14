e497fd293773a5801a42614d92249c75c5ef8f5b
lucasflaquer <lucas.flaquer@gmail.com>
2020-03-14 00:26:56 -0300

    #1h1g6y[REVISÃO] concluido

--- atividade1/src/main/java/com/example/atividade1/controller/ProductController.java
+++ atividade1/src/main/java/com/example/atividade1/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.example.atividade1.controller;
 
+import java.util.ArrayList;
+
 import com.example.atividade1.model.Product;
 import com.example.atividade1.service.ProductService;
 
@@ -14,18 +16,43 @@ import org.springframework.web.servlet.ModelAndView;
  * UserController
  */
 @Controller
-@RequestMapping("/product")
 public class ProductController {
 
     @Autowired
     private ProductService ps;
 
 
-    @GetMapping("/{id}")
+    @GetMapping("/produto/{id}")
     public ModelAndView getProduct(@PathVariable("id") int id) {
         ModelAndView mv = new ModelAndView("view");
         Product prod = ps.getProductById(id);
         mv.addObject("product", prod);
+        return mv;
+    }
+
+    
+    @GetMapping("/produtosEmEstoque")
+    public ModelAndView getInStock()
+    {
+        ModelAndView mv = new ModelAndView("in-stock");
+        ArrayList<Product> products = ps.getProductsWithStock();
+        mv.addObject("products", products);
+        return mv;
+    }
+    @GetMapping("/produtosValorAcima/{value}")
+    public ModelAndView getInProductUp(@PathVariable("value") double value)
+    {
+        ModelAndView mv = new ModelAndView("biggerthan");
+        ArrayList<Product> products = ps.getProductsByValueUp(value);
+        mv.addObject("products", products);
+        return mv;
+    }
+    @GetMapping("/produtosValorAbaixo/{value}")
+    public ModelAndView getInProductDown(@PathVariable("value") double value)
+    {
+        ModelAndView mv = new ModelAndView("biggerthan");
+        ArrayList<Product> products = ps.getProductsByValueDown(value);
+        mv.addObject("products", products);
         return mv;
     }
 }
