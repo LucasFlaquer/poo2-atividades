daae7c9c7a8cafd0063ca6878b0fd94605612f6d
lucasflaquer <lucas.flaquer@crbconstrutora.com.br>
2020-04-06 21:56:12 -0300

    #1k3a2k[REVIEW] entidade criada

---
+++ atividade2/src/main/java/com/example/atividade2/entity/Funcionario.java
@@ -0,0 +1,67 @@
+package com.example.atividade2.entity;
+
+public class Funcionario {
+
+  private int id;
+  private String nome;
+  private String cpf;
+  private String email;
+  private double salario;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public double getSalario() {
+    return salario;
+  }
+
+  public void setSalario(double salario) {
+    this.salario = salario;
+  }
+
+  public Funcionario() {}
+
+  public Funcionario(int id, String nome, String cpf, String email, double salario) {
+    this.id = id;
+    this.nome = nome;
+    this.cpf = cpf;
+    this.email = email;
+    this.salario = salario;
+  }
+
+  @Override
+  public String toString() {
+    return "Funcionario [cpf=" + cpf + ", email=" + email + ", id=" + id + ", nome=" + nome + ", salario=" + salario
+        + "]";
+  }
+    
+}
