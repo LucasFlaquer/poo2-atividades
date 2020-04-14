6feeba4afe7f8df19dc92c26f7237548c87426dd
lucasflaquer <lucas.flaquer@crbconstrutora.com.br>
2020-04-13 22:31:43 -0300

    #1k3a1h listagem e cadastro

--- atividade2/src/main/java/com/example/atividade2/controller/FuncionarioController.java
+++ atividade2/src/main/java/com/example/atividade2/controller/FuncionarioController.java
@@ -1,10 +1,12 @@
 package com.example.atividade2.controller;
 
+import com.example.atividade2.entity.Funcionario;
 import com.example.atividade2.service.FuncionarioService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,14 +18,14 @@ public class FuncionarioController {
   
   @GetMapping("/funcionarios")
   public ModelAndView index() {
-    ModelAndView mv = new ModelAndView("alunos");
+    ModelAndView mv = new ModelAndView("index");
     mv.addObject("funcionarios", service.getFuncionarios());
     return mv;
   }
 
   @PostMapping("/funcionarios")
-  public String store() {
-    
+  public String store(@ModelAttribute Funcionario func) {
+    service.salvar(func);
     return "redirect:/funcionarios";
   }
 
